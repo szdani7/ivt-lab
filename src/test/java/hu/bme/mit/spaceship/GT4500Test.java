dae4374c20aaa9ebd97c4e5e41b8f31ba87d16f8
szdani7 <szdani7@gmail.com>
2018-05-18 02:49:45 -0700

    Add GT4500Test.java

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,66 @@ import org.junit.Test;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore primaryTS;
+  private TorpedoStore secondaryTS;
 
   @Before
   public void init(){
-    this.ship = new GT4500();
+	  this.primaryTS = mock(TorpedoStore.class);
+	  this.secondaryTS = mock(TorpedoStore.class);
+	  this.ship = new GT4500(primaryTS, secondaryTS);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+	  when(primaryTS.fire(1)).thenReturn(true);
     // Act
-    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+	  boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
-    assertEquals(true, result);
+	// Verifying the mock
+	  assertEquals(true, result);
+	  verify(primaryTS, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+	  when(primaryTS.isEmpty()).thenReturn(false);
+	  when(primaryTS.fire(1)).thenReturn(true);
+	  when(secondaryTS.isEmpty()).thenReturn(false);
+	  when(secondaryTS.fire(1)).thenReturn(true);
     // Act
-    boolean result = ship.fireTorpedo(FiringMode.ALL);
+	  boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
-    assertEquals(true, result);
+	// Verifying the mock
+	  assertEquals(true, result);
+	  verify(primaryTS, times(1)).fire(1);
+	  verify(primaryTS, times(1)).fire(1);
   }
-
+  
+  @Test
+  public void fireTorpedo_Single_primaryFiredLast() {
+	  // Arrange
+	  when(primaryTS.isEmpty()).thenReturn(false);
+	  when(secondaryTS.isEmpty()).thenReturn(false);
+	  // Act
+	  ship.fireTorpedo(FiringMode.SINGLE);
+	  ship.fireTorpedo(FiringMode.SINGLE);
+	  // Verifying
+	  verify(primaryTS, times(1)).fire(1);
+	  verify(secondaryTS, times(1)).fire(1);
+  }
+  
+  @Test
+  public void fireTorpedo_Single_primaryFiredLast_else() {
+	  // Arrange
+	  
+	  // Act
+	  
+	  // Verifying
+	  
+  }
 }
